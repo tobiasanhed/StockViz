adda44930806898d518d86b7e5d437fe17157de1
Philip Arvidsson <philip@philiparvidsson.com>
2017-01-12 18:12:09 +0100

    back button on add-new-investments view

--- src/views/NewInvestments.tsx
+++ src/views/NewInvestments.tsx
@@ -3,14 +3,16 @@
  *------------------------------------*/
 
 import * as React from 'react'
+import { browserHistory } from 'react-router'
 
 import { CardTitle, Card, CardActions, CardHeader, CardText, FlatButton, FloatingActionButton } from 'material-ui'
-import ContentAdd from 'material-ui/svg-icons/content/add'
+import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
 import ContentEdit from 'material-ui/svg-icons/editor/mode-edit'
 import { Portfolio } from '../models/Portfolio'
 import {GridList, GridTile} from 'material-ui/GridList'
 import { ProviderFactory } from '../models/providers/ProviderFactory'
 import CircularProgress from 'material-ui/CircularProgress'
+import { Link } from 'react-router'
 
 /*--------------------------------------
  * CLASSES
@@ -23,7 +25,10 @@ export class NewInvestments extends React.Component<any, any> {
 
     render() {
         return <div style={{ width : '100%' }}>
-        				hej
-        		</div>
+                   <FloatingActionButton onTouchTap={browserHistory.goBack}
+                                         style={{position: 'absolute', left: 20, top: 20}}>
+                       <NavigationArrowBack />
+                   </FloatingActionButton>
+               </div>
     }
 }
