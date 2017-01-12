295d16b98726ccdc75d28d941708050a98fc807a
Philip Arvidsson <philip@philiparvidsson.com>
2017-01-12 11:21:40 +0100

    initial stuff

---
+++ src/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow() {
+    win = new BrowserWindow({ width: 640, height: 480 })
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
