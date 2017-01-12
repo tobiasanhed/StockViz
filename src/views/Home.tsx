929265da7e0ca56ea15871e45aa17ccf2c0e8a73
Philip Arvidsson <philip@philiparvidsson.com>
2017-01-12 15:11:49 +0100

    .

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -11,12 +11,21 @@ import ContentAdd from 'material-ui/svg-icons/content/add'
  * CLASSES
  *------------------------------------*/
 
+class InvestmentCard extends React.Component<any, { title: string }> {
+
+    title: string
+    render() {
+        return <Card><CardTitle>{this.bosse}</CardTitle> blablalba</Card>
+    }
+}
+
 export class Home extends React.Component<any, any> {
     /*--------------------------------------
      * METHODS
      *------------------------------------*/
 
     render() {
-        return <div><FlatButton label="bo2e34sse"/></div>
+        cards[0].title =" bosse"
+        return <div><InvestmentCard title="hej"/></div>
     }
 }
