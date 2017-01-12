9242aa7af24e887a135bb7603cb7baebf5dcbf3d
Philip Arvidsson <philip@philiparvidsson.com>
2017-01-12 13:05:58 +0100

    added first providers

---
+++ src/models/Investment.ts
@@ -0,0 +1,24 @@
+/*--------------------------------------
+ * IMPORTS
+ *------------------------------------*/
+
+import { Price } from './Price'
+
+/*--------------------------------------
+ * CLASSES
+ *------------------------------------*/
+
+export class Investment {
+    /*--------------------------------------
+     * FIELDS
+     *------------------------------------*/
+
+    amount: number
+
+    initialPrice: number
+
+    investmentName: string
+    providerName: string
+
+    imageURL: string
+}
