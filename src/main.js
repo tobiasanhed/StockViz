1557fc01cd6594ad37a945365dca6c3188da1a80
Philip Arvidsson <philip@philiparvidsson.com>
2017-02-08 13:26:59 +0100

    dont open dev tools

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ function createWindow() {
     }
 
     win.setMenu(null)
-    win.webContents.openDevTools()
+    //win.webContents.openDevTools()
 
     win.on('closed', () => {
         win = null
